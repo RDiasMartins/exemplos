127009581214647a850f5e9518487b802caddeed
RDiasMartins <renan2204@outlook.com>
2020-03-10 17:48:10 -0300

    Exemplos WEB 10.03.2020

---
+++ 2020.03.10/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="utf-8">
+        <title> Index </title>
+    </head>
+    <body>
+        <a href="ex_onchange.html"> Exemplo Onchange </a> <br/>
+        <a href="ex_onclick.html"> Exemplo Onclick </a> <br/>
+        <a href="ex_onblur.html"> Exemplo Onblur </a> <br/>
+        <a href="ex_onfocus.html"> Exemplo Onfocus </a> <br/>
+        <a href="ex_onmouseover_mouseout.html"> Exemplo MouseOver & Out </a> <br/>
+        <a href="ex_onkeydown_kewyup.html"> Exemplo OnKeyDown & Up </a> <br/>
+        <a href="ex_onload.html"> Exemplo OnLoad </a>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,34 +1,39 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8" />
-    <title> Exemplos em aula - Renan </title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="bootstrap.min.css" />
-    <link rel="stylesheet" type="text/css" href="estilo.min.css" />
-    <link href = "https://fonts.googleapis.com/icon?family=Material+Icons" rel = "stylesheet"/>
-</head>
-<body>
-    <div class="container-fluid">
-        <?php
-        include "menu.inc";
-        ?>
-        <br/>
+    <head>
+        <meta charset="utf-8" />
+        <title> Exemplos em aula - Renan </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="estilo.min.css" />
+        <link href = "https://fonts.googleapis.com/icon?family=Material+Icons" rel = "stylesheet"/>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <?php
+            include "menu.inc";
+            ?>
+            <br/>
 
-        <table class="table table-light table-striped table-hover">
-            <thead class="thead-dark">
-                <tr>
-                    <th>#</th>
-                    <th>Data</th>
-                    <th>Exemplo</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>03-03-2020</td>
-                    <td> <a href="2020.03.03/index.php"> JavaScript </a> </td>
-                </tr>
-            </tbody>
-</body>
+            <table class="table table-light table-striped table-hover">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>#</th>
+                        <th>Data</th>
+                        <th>Exemplo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>03-03-2020</td>
+                        <td> <a href="2020.03.03/index.php"> JavaScript </a> </td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>10-03-2020</td>
+                        <td> <a href="2020.03.10/index.php"> Gatilhos JS </a> </td>
+                    </tr>
+                </tbody>
+    </body>
 </html>
